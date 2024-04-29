ca4e34c200788ffd6cc86cdd658cb392dd743057
lizandra.malta <lizlrmalta@gmail.com>
2024-04-29 01:47:46 -0300

    adicionando botões de ação ao content

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -2,18 +2,64 @@ import {
   KeyboardAwareScrollView,
   KeyboardAwareScrollViewProps,
 } from "react-native-keyboard-aware-scroll-view";
-import { ContentComponent, ContentStyleProps } from "./styles";
+import {
+  ContentComponent,
+  ContentStyleProps,
+  SecondaryButtonContainer,
+} from "./styles";
+import { Button } from "@components/button";
+
+type ContentProps = {
+  primaryButton?: {
+    text: string;
+    action: () => void;
+    icon?: PhosphorIcon;
+  };
+  secondaryButton?: {
+    text: string;
+    action: () => void;
+    icon?: PhosphorIcon;
+  };
+};
 
 export function Content({
+  primaryButton,
+  secondaryButton,
   rounded,
   children,
   ...rest
-}: ContentStyleProps & KeyboardAwareScrollViewProps) {
+}: ContentProps & ContentStyleProps & KeyboardAwareScrollViewProps) {
   return (
     <ContentComponent rounded={rounded}>
       <KeyboardAwareScrollView {...rest} showsVerticalScrollIndicator={false}>
         {children}
       </KeyboardAwareScrollView>
+      {primaryButton && (
+        <Button
+          buttonTheme="DARK"
+          label={primaryButton.text}
+          icon={
+            primaryButton.icon
+              ? { name: primaryButton.icon, size: 18 }
+              : undefined
+          }
+          onPress={primaryButton.action}
+        />
+      )}
+      {secondaryButton && (
+        <SecondaryButtonContainer>
+          <Button
+            buttonTheme="LIGHT"
+            label={secondaryButton.text}
+            icon={
+              secondaryButton.icon
+                ? { name: secondaryButton.icon, size: 18 }
+                : undefined
+            }
+            onPress={secondaryButton.action}
+          />
+        </SecondaryButtonContainer>
+      )}
     </ContentComponent>
   );
 }
--- src/components/content/styles.ts
+++ src/components/content/styles.ts
@@ -7,9 +7,15 @@ export type ContentStyleProps = {
 export const ContentComponent = styled.View<ContentStyleProps>`
   flex: 1;
   padding: 40px 24px;
+  justify-content: space-between;
   ${({ rounded = false, theme }) => css`
     background-color: ${theme.COLORS.GRAY_700};
     border-top-right-radius: ${rounded ? 20 : 0}px;
     border-top-left-radius: ${rounded ? 20 : 0}px;
   `}
+`;
+
+export const SecondaryButtonContainer = styled.View`
+  flex: 1;
+  margin-top: 8px;
 `;
